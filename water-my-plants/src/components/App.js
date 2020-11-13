18ca175b273cce744f2550d3f9e085f4a20234ea
Olivia Toolson <livytoolson@gmail.com>
2020-11-13 12:33:39 -0700

    removed cra files that were not needed

--- water-my-plants/src/components/App.js
+++ water-my-plants/src/components/App.js
@@ -1,13 +1,10 @@
-import './App.css';
+import '../App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
+        <p>Water My Plants Build Week</p>
       </header>
     </div>
   );
