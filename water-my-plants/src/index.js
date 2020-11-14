43f028b7198495d4bf4f894cc89fc6ebc454f1f4
Olivia Toolson <livytoolson@gmail.com>
2020-11-13 17:29:34 -0700

    created store, skeleton of reducer, and provider

--- water-my-plants/src/index.js
+++ water-my-plants/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
+import reducer from './store/reducers';
 import App from './components/App';
 
+const store = createStore(reducer, applyMiddleware( thunk ));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
