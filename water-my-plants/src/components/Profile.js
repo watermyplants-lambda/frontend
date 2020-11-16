c18c5a6781bd0f21ef798e213fb46ac0a28420c9
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-16 12:57:38 -0600

    created basic update form

--- water-my-plants/src/components/Profile.js
+++ water-my-plants/src/components/Profile.js
@@ -1,8 +1,53 @@
-import React from 'react';
-// import { axiosWithAuth } from '../utils/axiosWithAuth';
+import React, {useState, useEffect} from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Profile = () => {
 
+const initialUserValues = {
+    username: '',
+    email:'',
+    password: ''
+}
+
+const Profile = () => { 
+    const [update, setUpdate] = useState(false)
+    const [userValues, setUserValues] = useState(initialUserValues)
+
+    useEffect(() => {
+        axiosWithAuth()
+        .get('/') //need endpoint
+        .then((res) => {
+            setUserValues(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    const saveNewInfo = (e) => {
+        e.preventDefault()
+        axiosWithAuth()
+        .put('/', userValues) //need endpoint
+        .then((res) => {
+            setUpdate(false)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const handleChange = (e) => {
+        e.persist()
+        setUserValues({
+            ...userValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onClickEdit = (e) => {
+        setUpdate(true)
+    }
+
+    
     // need to be able to update phone number and password
     // user must be autenticated 
     // use axiosWithAuth
@@ -12,7 +57,39 @@ const Profile = () => {
 
     return (
         <div>
-            User Profile
+            <h1>User Profile</h1>
+                <h3>Username:{userValues.username}</h3>
+                <h3>Email:{userValues.email}</h3>
+                <button onClick = {onClickEdit}>Update My Info</button>
+            {update && (
+                <form onSubmit = {saveNewInfo}>
+                    <label>Username:
+                        <input
+                            type = 'text'
+                            name = 'username'
+                            value = {userValues.username}
+                            onChange = {handleChange}
+                        />
+                    </label>
+                    <label>Email:
+                        <input
+                            type = 'text'
+                            name = 'email'
+                            value = {userValues.email}
+                            onChange = {handleChange}
+                        />
+                    </label>
+                    <label>Password:
+                        <input
+                            type = 'password'
+                            name = 'password'
+                            value = {userValues.password}
+                            onChange = {handleChange}
+                        />
+                    </label>
+                    <button>Save Info</button>
+                </form>
+            )}
         </div>
     )
 };
