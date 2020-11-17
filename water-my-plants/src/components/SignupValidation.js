773aa4e352776e3be5586b847a89a0ec8dde4c1f
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-17 13:30:09 -0600

    installed dependencies

--- water-my-plants/src/components/SignupValidation.js
+++ water-my-plants/src/components/SignupValidation.js
@@ -1,18 +1,18 @@
-import * as yup from 'yup';
+// import * as yup from 'yup';
 
-export default yup.object().shape({
-    firstName: yup
-    .string()
-    .required("A first name is required"),
-    lastName: yup
-    .string()
-    .required("A last name is required"),
-    email: yup
-    .string()
-    .email('Please enter a valid email adress')
-    .required("A email is required"),
-    password: yup
-    .string()
-    .required("A password is required")
-    .min(9, 'Password must be a minimim of 9 chars '),
-});
+// export default yup.object().shape({
+//     firstName: yup
+//     .string()
+//     .required("A first name is required"),
+//     lastName: yup
+//     .string()
+//     .required("A last name is required"),
+//     email: yup
+//     .string()
+//     .email('Please enter a valid email adress')
+//     .required("A email is required"),
+//     password: yup
+//     .string()
+//     .required("A password is required")
+//     .min(9, 'Password must be a minimim of 9 chars '),
+// });
