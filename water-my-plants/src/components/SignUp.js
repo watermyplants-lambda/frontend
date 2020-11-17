4f3b0be9cf3ef77e54ce90700140f9a032068d46
Diego Roman <diegormnv209@gmail.com>
2020-11-17 09:31:56 -0800

    There should be changes now

--- water-my-plants/src/components/SignUp.js
+++ water-my-plants/src/components/SignUp.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import axiosWithAuth from '../utils/axiosWithAuth'; 
+import * as yup from 'yup';
+import SignUpValidation from './SignupValidation';
 
 const initialSignup ={
     firstName: '',
@@ -7,19 +9,42 @@ const initialSignup ={
     email: '',
     password: ''
 }
+
+const initialSignupError ={
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+}
+
 const initialUsers=[];
+const initialSignupDisabled = true;
 
 const SignUp () => {
-    const [signupForm, setSignup] = useState(initialSignup);
+    const [signupForm, setSignupForm] = useState(initialSignup);
     const [users, setUsers] = useState(initialUsers);
+    const [signupFormErrors, setSignupFormErrors] = useState(initialSignupError);
+    const [signupDisabled, setSignupDisabled] = useState(initialSignupDisabled)
+
+    useEffect(() =>{
+        axios
+        .get('https://watermyplants35.herokuapp.com/auth/register') //may need to change api
+        .then((res) =>{
+            setUsers(initialUsers)
+        })
+        .catch((err) =>{
+            console.log(err,'error')
+        })
+    },[]);
 
 
     const saveNewUser = (newUser) =>{
         axios
-        .post('/', newUser)
-        .then((res) =>{
+        .post('https://watermyplants35.herokuapp.com/auth/register', newUser) //may need to change api
+        .then((res) => {
             setUsers([res.data, ...users]);
             setSignup(initialSignup);
+            //should this redirect to profile or home if successful
         })
         .catch((err) =>{
             console.log(err, 'error')
@@ -27,64 +52,112 @@ const SignUp () => {
     }
 
     const submitSignup = () ={
-        const newUser ={
+        const newUser ={   //need to fix error of ':' expected for newUser
             firstName: signupForm.firstName.trim(),
             lastName: signupForm.lastName.trim(),
             email: signupForm.email.trim(),
             password: signupForm.password.trim(),
         }
-        saveNewUser(newUser);
+        saveNewUser(newUser);  //need to fix this error of ',' expected for saveNewUser
+    }
+
+    const changeSignup = (name, value) =>{
+        //validation
+        yup.reach(SignUpValidation, name)
+        .validate(value)
+        .then(() =>{
+            //console.log(value)
+            setSignupFormErrors({
+                ...signupFormErrors,
+                [name]: '',
+            });
+        })
+        .catch((err) =>{
+            setSignupFormErrors,
+            [name]; err.errors[0],
+        });
+        setSignupForm({...setSignupForm, [name]: value});
+    }
+    
+    useEffect(() =>{
+        SignUpValidation.isValid(signupForm).then((valid) =>{
+            setSignupDisabled(!valid);
+        });
+    },[signupForm]);
+
+    const onSubmit = evt =>{
+        evt.preventDefault();
+        submitSignup();
+    }
+
+    //Need to add to check if login info is correct to be successful or not
+
+    const onChange = evt =>{
+        const { name, value, type, checked } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse)
     }
 
 
     return(
-        <form className='signup form' onSubmit={}>
+        <form className='signup form' onSubmit={onSubmit}>
             <div>
                 <p>Sign Up</p>
             </div>
-            <div className='signup'>
-                <label>First Name
-                <input 
-                            type='text'
-                            name='firstName'
-                            value={signupForm.firstName}
-                        />
-                    </label>
+            <div className='signuperrors'>
 
-                    <label>Last Name
-                        <input 
-                            type='text'
-                            name='lastName'
-                            value={signupForm.lastName}
-                        />
-                    </label>
-
-                    <label>Email
-                        <input 
-                            type='text'
-                            name='email'
-                            value={signupForm.email}
-                        />
-                    </label>
-
-                    <label>Password
-                        <input 
-                            type='text'
-                            name='password'
-                            value={signupForm.password}
-                        />
-                    </label>
-
-                    <button className='loginBttn'>Login</button>
-                
-                </label>
-            
+            <div className='errors'>
+                <p>{signupFormErrors.firstName}</p>
+                <p>{signupFormErrors.lastName}</p>
+                <p>{signupFormErrors.email}</p>
+                <p>{signupFormErrors.password}</p>
+            </div>
             
             
             </div>
-        
-        
-        
+            <div className='signup'>
+                <label>First Name
+                    <input 
+                        type='text'
+                        name='firstName'
+                        value={signupForm.firstName}
+                        onChange={onChange}
+                        submit={submitSignup}
+                    />
+                </label>
+
+                <label>Last Name
+                    <input 
+                        type='text'
+                        name='lastName'
+                        value={signupForm.lastName}
+                        onChange={changeSignup}
+                        submit={onChange}
+                    />
+                </label>
+
+                <label>Email
+                    <input 
+                        type='text'
+                        name='email'
+                        value={signupForm.email}
+                        onChange={changeSignup}
+                        submit={onChange}
+                    />
+                </label>
+
+                <label>Password
+                    <input 
+                        type='text'
+                        name='password'
+                        value={signupForm.password}
+                        onChange={changeSignup}
+                        submit={onChange}
+                    />
+                </label>
+
+                <button className='signupBttn' disabled={signupDisabled} >Submit</button>
+            </div>
         </form>
     )
 
